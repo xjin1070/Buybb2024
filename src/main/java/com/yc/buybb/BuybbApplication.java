ef9b379f7347c1b65de1178e539b6a69d5811f27
xjin1 <xjin1070@gmail.com>
2023-03-18 14:18:42 +0800

    修复接口数据异常bug

---
+++ src/main/java/com/yc/buybb/BuybbApplication.java
@@ -0,0 +1,13 @@
+package com.yc.buybb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuybbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuybbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yc/buybb/BuybbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.buybb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuybbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
